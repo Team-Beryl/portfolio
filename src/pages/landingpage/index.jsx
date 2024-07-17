b4a507708dd41b65007abc6b86382858126e05de
Elsie <elsietani117@gmail.com>
2024-07-17 14:21:19 +0000

    resolve conflicts

--- src/pages/landingpage/index.jsx
+++ src/pages/landingpage/index.jsx
@@ -4,17 +4,7 @@ import { left, right } from "../../assets"
 const LandingPage = () => {
     return (
         <div>
-            <div>
-
-      <div>
-        <img src={} alt="" />
       <img src= {left} alt="" />
-      </div>
-
-
-            </div>
-Landing
-        
         </div>
     )
 }

877b6e4e94266b3c44af61df1013f6757c45a08f
Elsie <elsietani117@gmail.com>
2024-07-17 14:31:11 +0000

    resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -25,28 +25,6 @@ import AddVolunteering from "./pages/dashboard/pages/addvolunteering";
 
 
 
-function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <LandingPage />
-import UserProfile from "./pages/dashboard/pages/userprofile";
-import DashboardHome from "./pages/dashboard/pages/home";
-import AddSkill from "./pages/dashboard/pages/addSkill";
-import AddProfile from "./pages/dashboard/pages/addprofile";
-import AddProject from "./pages/dashboard/pages/addproject";
-import AddExperience from "./pages/dashboard/pages/addexperience";
-import AddAcheivement from "./pages/dashboard/pages/addacheivement";
-import Education from "./pages/dashboard/pages/education";
-import Volunteering from "./pages/dashboard/pages/volunteering";
-import AddEducation from "./pages/dashboard/pages/addeducation";
-import AddVolunteering from "./pages/dashboard/pages/addvolunteering";
-
-
-
-
-
-
 
 function App() {
   const router = createBrowserRouter([
@@ -54,7 +32,6 @@ function App() {
       path: "/",
       element: <LandingPage />
 
-    },
     },
 
     {
@@ -64,13 +41,9 @@ function App() {
     {
       path: "dashboard/profile",
       element: <UserProfile />
-      path: "dashboard/profile",
-      element: <UserProfile />
     },
 
     {
-      path: "dashboard/skills",
-      element: <Skills />
       path: "dashboard/skills",
       element: <Skills />
     },
@@ -78,31 +51,18 @@ function App() {
     {
       path: "dashboard/projects",
       element: <Projects />
-      path: "dashboard/projects",
-      element: <Projects />
     },
 
     {
       path: "dashbaord/experiences",
       element: <Experience />
-      path: "dashbaord/experiences",
-      element: <Experience />
     },
-
-
-
-
-
     {
-      path: "dashboard/acheivements",
-      element: <Acheivement />
       path: "dashboard/acheivements",
       element: <Acheivement />
     },
 
     {
-      path: "dashboard/education",
-      element: <Education />
       path: "dashboard/education",
       element: <Education />
     },
@@ -112,27 +72,19 @@ function App() {
       element: <Volunteering />
     },
 
-      path: "dashboard/volunteering",
-      element: <Volunteering />
-    },
-
-
     {
       path: "prev",
       element: <Preview />
     },
-    {
-      path: "prev",
-      element: <Preview />
-    },
+  
 
     {
-      path: "sign",
+      path: "signup",
       element: <SignUp />
     },
 
     {
-      path: "sig",
+      path: "signin",
       element: <Signin />
     },
 
@@ -170,54 +122,10 @@ function App() {
       path: "dashboard/education/addeducation",
       element: <AddEducation />
     },
-
-    {
-      path: "sig",
-      element: <Signin />
-    },
-
-    {
-      path: "dashboard/skills/add-skill",
-      element: <AddSkill />
-    },
-
-    {
-      path: "dashboard/profile/addprofile",
-      element: <AddProfile />
-    },
-
-    {
-      path: "dashboard/projects/addproject",
-      element: <AddProject />
-    },
-
-    {
-      path: "dashboard/experiences/addexperience",
-      element: <AddExperience />
-    },
-
-    {
-      path: "dashboard/acheivements/addacheivement",
-      element: <AddAcheivement />
-    },
-
-    {
-      path: "dashboard/volunteering/addvolunteering",
-      element: <AddVolunteering />
-    },
-
-    {
-      path: "dashboard/education/addeducation",
-      element: <AddEducation />
-    }
-
 
   ])
 
 
-  return (
-    <RouterProvider router={router} />
-  )
   return (
     <RouterProvider router={router} />
   )
--- src/pages/landingpage/index.jsx
+++ src/pages/landingpage/index.jsx
@@ -2,18 +2,6 @@ import { left, right } from "../../assets"
 
 
 const LandingPage = () => {
-  return (
-    <div>
-
-      <div>
-        <img src={} alt="" />
-      <img src= {left} alt="" />
-      </div>
-
-
-
-    </div>
-  )
     return (
         <div>
       <img src= {left} alt="" />
